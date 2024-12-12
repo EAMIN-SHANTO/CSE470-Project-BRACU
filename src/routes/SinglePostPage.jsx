2f195fbea45d1b81e46808cb8ff4966dcca95246
eamin <md.eamin519@gmail.com>
2024-12-13 01:27:04 +0600

    single post work start

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,7 +1,25 @@
+import Image from "../components/Image";
+import { Link } from "react-router-dom";
 const SinglePostPage = () => {
     return (
-      <div className="">
-        <h1>SinglePostPage</h1>
+      <div className="flex flex-col gap-8">
+       {/* details */}
+       <div className="flex gap-8">
+        <div className="lg:w-3/5 flex flex-col gap-8">
+          <h1>starting of sing post writing here is how</h1>
+          <span> Written By</span>
+          <Link> </Link>
+        </div>
+        <div className="hidden lg:block w-2/5">
+          <Image src="postImg.jpeg"/>
+        
+        </div>
+
+       </div>
+       {/* content */}
+       <div className="">
+
+       </div>
       </div>
     )
   }
